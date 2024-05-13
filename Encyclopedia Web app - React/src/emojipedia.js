ef9b20446cda37e3c64a3849b685590a5df9f437
Premanshc <premanshacademics@gmail.com>
2024-05-13 15:48:45 +0530

    add emojipedia web app

---
+++ Encyclopedia Web app - React/src/emojipedia.js
@@ -0,0 +1,82 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🙉️",
+    name: "Hear-No-Evil Monkey",
+    meaning:
+    "Monkey with two hands in front of its eyes. Closes its eyes to the world so as not to see anything bad or cruel, or is embarrassed. Mizaru is one of the three wise monkeys. In a sense, ignoring bad things."
+  },
+  {
+    id: 5,
+    emoji: "🥬️",
+    name: "Leafy vegetables",
+    meaning:
+    "I eat healthy food or “Today I am going to cook an Asian dish“. Be it diet, vegetarian nutrition or side dish with a steak. With this type of vegetables you eat both stem and leaf. Can be used for salad, chard, spinach, or Chinese cabbage."
+  },
+  {
+    id: 6,
+    emoji: "🐸️",
+    name: "Frog Face",
+    meaning:
+    "I have a frog in the throat! In mythology, stands for fertility, death and rebirth. Can also be seen in connection with prince charming (kiss the frog). Also known as Kermit, the frog from the Muppet Show."
+  },
+  {
+    id: 7,
+    emoji: "🍋️",
+    name: "Lemon",
+    meaning:
+    "The lemon 🍋 a yellow citrus fruit, is known for its tart taste and refreshing effect. As a healthy fruit with a high vitamin C content, it is a valuable part of a balanced diet. Sayings such as “When life gives you a lemon, make lemonade“ illustrate the ability to make the best of difficult situations and maintain a positive attitude."
+  },
+  {
+    id: 8,
+    emoji: "🍈️",
+    name: "Melon",
+    meaning:
+    "The juicy melon is considered a symbol for eroticism and lucky love. Can also be interpreted as a bad omen."
+  },
+  {
+    id: 9,
+    emoji: "🫐️",
+    name: "Blueberries",
+    meaning:
+    "We're going to pick blueberries in the forest! The blue summer fruit is one of the local super-foods. They make your tongue blue. Whether in a fruit salad, as a topping for muesli or in a cake. Often related to healthy eating."
+  },
+  {
+    id: 10,
+    emoji: "🍉️",
+    name: "Watermelon",
+    meaning:
+    "The juicy fruit stands for hot summer days, stone spitting contests, healthy eating and holidays. The emoticon of the watermelon promises bliss of love. It is also said to have a positive effect on potency."
+  },
+  {
+    id: 11,
+    emoji: "🥒️",
+    name: "Cucumber",
+    meaning:
+    "I’ll have just salad today! The vegetable is low in calories and represents a healthy diet. As a beauty recipe, the slices of the cut cucumber can be placed on swollen eyes."
+  },
+  
+];
+
+export default emojipedia;
